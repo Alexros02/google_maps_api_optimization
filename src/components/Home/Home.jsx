912ed8b8996e0567dd9661403ff2148b28af4c7d
alexr <alexrosmmii.ar@gmail.com>
2025-02-24 19:30:46 +0100

    home edited

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,9 @@ import React from "react";
 const Home = () => {
 return (
     <div>
-        <h1>Home Page</h1>
-    </div>);
+       < h1>Welcome to the Home Page</h1>
+    </div>
+);
 };
 
 export default Home;
