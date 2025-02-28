5cbb683bc4a9a7f33aeca178c5e19ea5959224ae
alexr <alexrosmmii.ar@gmail.com>
2025-02-28 19:19:56 +0100

    exported styles config for SweetAlert.jsx

--- src/components/SweetAlert/SweetAlert.jsx
+++ src/components/SweetAlert/SweetAlert.jsx
@@ -1,54 +1,8 @@
+import React from 'react';
 import Swal from 'sweetalert2';
-import {Plus}from "lucide-react"
-import {addDoc, collection} from "firebase/firestore";
-import {db} from "../../firebaseConfig";
-import {addPost} from "../../Services/PostsService";
-
-const customSwalStyles = `
-.swal2-custom {
-  background-color: #FAF3DD;
-  color: #2C2C2E;
-  border: none;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-}
-
-.swal2-custom .swal2-title {
-  color: #2C3E50;
-}
-
-.swal2-custom .swal2-html-container {
-  color: #2C2C2E;
-}
-
-.swal2-custom .swal2-confirm {
-  background-color: #D4AF37;
-  color: #2C2C2E;
-}
-
-.swal2-custom .swal2-cancel {
-  background-color: #2C3E50;
-  color: #FAF3DD;
-}
-
-.swal2-success .swal2-icon {
-  border-color: #C1E1C1;
-  color: #C1E1C1;
-}
-
-.swal2-error .swal2-icon {
-  border-color: #F5D0C5;
-  color: #F5D0C5;
-}
-
-.swal2-warning .swal2-icon {
-  border-color: #FDE047;
-  color: #FDE047;
-}
-`;
-
-const style = document.createElement('style');
-style.textContent = customSwalStyles;
-document.head.appendChild(style);
+import { Plus } from 'lucide-react';
+import { addPost } from '../../Services/PostsService';
+import './SweetAlert.css';
 
 const SweetAlert = ({ title, onPostAdded }) => {
     const show = () => {
@@ -61,7 +15,6 @@ const SweetAlert = ({ title, onPostAdded }) => {
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label"><span class="label-text">Latitud</span></label>
-                    <input type="number" id="lat" step="any" placeholder="Ej: 40.7128" class="input input-bordered w-full max-w-xs" />
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label"><span class="label-text">Longitud</span></label>
@@ -71,6 +24,9 @@ const SweetAlert = ({ title, onPostAdded }) => {
             showCancelButton: true,
             confirmButtonText: "Guardar",
             cancelButtonText: "Cancelar",
+            customClass: {
+                popup: 'swal2-custom'
+            },
             preConfirm: async () => {
                 const direccion = Swal.getPopup().querySelector("#direccion").value;
                 const lat = parseFloat(Swal.getPopup().querySelector("#lat").value.trim());
